71e33d6a0cb31dae1173e60f59f1b8ae35495aa5
yagomuniz <yagomunizns@gmail.com>
2018-08-27 15:30:58 -0300

    Implementado exercício sobre Singleton

---
+++ Exercicio03/Conexao.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class Conexao{
+
+    private Conexao(){ }
+    private Connection conn;
+    private static Conexao instance;
+
+    public static Conexao getInstance(){
+        if(instance == null){
+            instance = new Conexao();
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                instance.conn = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1/banco","usuario","senha");
+                
+            } catch (SQLException e) {
+                System.out.println(e);       
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection(){
+        return this.conn;
+    }
+}
